2ba3e563292374b7e8b73de7d712c5c762794efa
mafilala <marufsherefa9@gmail.com>
2024-01-11 17:14:32 +0300

    refined bot_instance to work on remote host

--- Maruf/bot/bot_instance.py
+++ Maruf/bot/bot_instance.py
@@ -5,15 +5,14 @@ from aiogram import Bot
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN_API = os.environ.get("TOKEN_API")
-# For hosting on pythonanywhere use the following commented code
-# from aiogram.client.session.aiohttp import AiohttpSession
-# bot = Bot(
-#     token=TOKEN_API,
-#     parse_mode='HTML',
-#     session=AiohttpSession(proxy='http://proxy.server:3128')
-# )
-
+from aiogram.client.session.aiohttp import AiohttpSession
 bot = Bot(
     token=TOKEN_API,
-    parse_mode='HTML'
+    parse_mode='HTML',
+    session=AiohttpSession(proxy='http://proxy.server:3128')
 )
+
+# bot = Bot(
+#     token=TOKEN_API,
+#     parse_mode='HTML'
+# )
